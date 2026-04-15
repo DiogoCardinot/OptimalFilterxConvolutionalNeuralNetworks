5156ca645b02fe6da6e8079519e649e8325a9dc7
DiogoCardinot <diogocardinotuerj@gmail.com>
2026-04-15 09:42:23 -0300

    Adiciona a comparação entre as CNNs antigas e a nova CNN tanto para amplitude como para phase

--- Plots/CompareCNNHist/compareCNNHistograms.py
+++ Plots/CompareCNNHist/compareCNNHistograms.py
@@ -13,7 +13,87 @@ CNN= 3
 
 plt.rcParams['savefig.directory'] = os.path.dirname(path)
 
-def PlotHistrogramas():
+def PlotHistrogramasAmpitude():
+    base_path = os.path.dirname(os.path.dirname(path))
+    ocupacoes = [10,50,80,100]
+    total_inches_image = 6.32
+    fontSize = 24
+    fig, (ax) = plt.subplots(2, 2, figsize=(15, 6))
+    ax = ax.flatten()
+    cnn8_color ="#006130"
+
+    for idx, ocupacao in enumerate(ocupacoes):
+        if ocupacao == 10 or ocupacao==50:
+            CNN = 5
+            cnn_color = "#1A1A1A"
+        elif ocupacao==80 or ocupacao==100:
+            CNN=3
+            cnn_color = "#B0B0B0"
+
+        # CNN8
+        cnn8_data_path = os.path.join(base_path,f'RedeNeuralConvolucional', f'CNN_8',f'results_ocupacao_{ocupacao}.npz')      
+        cnn8_data = np.load(cnn8_data_path)
+        cnn8_error = cnn8_data['error']
+        #CNN
+        cnn_data_path = os.path.join(base_path,f'RedeNeuralConvolucional', f'CNN_{CNN}',f'results_ocupacao_{ocupacao}.npz')      
+        cnn_data = np.load(cnn_data_path)
+        cnn_error = cnn_data['error']
+        
+        bins = 150
+        ax[idx].hist(cnn8_error, bins = bins, alpha=0.7,histtype='step', color=cnn8_color, linewidth=2, linestyle='dotted')
+        ax[idx].hist(cnn_error, bins = bins, alpha=0.7,histtype='step', color=cnn_color, linewidth=2)
+        ax[idx].text(-0.15, 1.12, f'({chr(97+idx)})', transform=ax[idx].transAxes, fontsize=fontSize+6, va='top')
+        ax[idx].set_xlabel(f'Amplitude estimation error (ADC Counts)', fontsize=fontSize)
+        ax[idx].set_ylabel('Number of Events', fontsize=fontSize)
+        # ax[idx].legend(loc='best')
+        ax[idx].grid(True, alpha=0.3)
+        formatter = ScalarFormatter(useMathText=False)
+        formatter.set_scientific(True)
+        formatter.set_powerlimits((0, 0))
+        formatter.set_useOffset(True)
+        ax[idx].yaxis.set_major_formatter(formatter)
+        if idx==0:
+            ax[idx].set_xlim(-20,20)
+        elif idx==1:
+            ax[idx].set_xlim(-75,75)
+        else:
+            ax[idx].set_xlim(-100,100)
+        # elif idx==1:
+        #     ax[idx].set_xlim(-200,100)
+        # elif idx==2:
+        #     ax[idx].set_xlim(-300,100)
+        # elif idx==3:
+        #     ax[idx].set_xlim(-300,200)
+
+    handles = []
+    labels = []
+    
+    handles.append(plt.Line2D([0], [0], color=cnn8_color, linewidth=2, linestyle='dotted'))
+    labels.append('CNN-8')
+    cnn_types = ['CNN-3', 'CNN-5']
+    unique_cnns = list(set(cnn_types))
+    for cnn_type in unique_cnns:
+        if cnn_type == "CNN-5":
+            color = "#1A1A1A"
+        else: 
+            color = "#B0B0B0"
+       
+        handles.append(plt.Line2D([0], [0], color=color, linewidth=2))
+        labels.append(cnn_type)
+
+    fig.legend(
+        handles, labels,
+        loc='upper center',
+        ncol=len(handles),
+        bbox_to_anchor=(0.5, 0.9999),
+        frameon=False,
+        fontsize=fontSize
+    )
+    # plt.tight_layout()
+    plt.subplots_adjust(hspace=0.4)
+    plt.show()
+
+def PlotHistrogramasPhase():
     base_path = os.path.dirname(os.path.dirname(path))
     ocupacoes = [10,50,80,100]
     total_inches_image = 6.32
@@ -87,4 +167,178 @@ def PlotHistrogramas():
     plt.subplots_adjust(hspace=0.4)
     plt.show()
 
-PlotHistrogramas()
+
+def PlotErros():
+    ocupacoes = [10,50,80,100]
+    total_inches_image = 6.32
+    fontSize = 24
+    fig, (ax) = plt.subplots(2, 2, figsize=(15, 6))
+    ax = ax.flatten()
+    cnn8_color = "#006130"
+    real_amplitude_color = "#FA3232"
+    base_path = os.path.dirname(os.path.dirname(path))
+    plots_path = os.path.join(base_path, "Plots")
+    for idx, ocupacao in enumerate(ocupacoes):
+        if ocupacao == 10 or ocupacao==50:
+            CNN = 5
+            cnn_color = "#1A1A1A"
+        elif ocupacao==80 or ocupacao==100:
+            CNN=3
+            cnn_color = "#B0B0B0"
+
+        # CNN8
+        data_cnn8_path = os.path.join(plots_path, 'Dados', 'CNN_ErrorxRealPhase', f'janelamento_{n_janelamento}', 'CNN_8', f'errorxreal_{ocupacao}.npz')
+        data_cnn8 = np.load(data_cnn8_path, allow_pickle=True)
+        stats_por_intervalo_cnn8 = data_cnn8['stats_por_intervalo'].item()
+        stats_por_intervalo_cnn8_amplitude = data_cnn8['stats_por_intervalo_amplitude'].item()
+        # # CNN
+        data_cnn_path = os.path.join(plots_path, "Dados", "CNN_ErrorxRealPhase",f'janelamento_{n_janelamento}', f'CNN_{CNN}', f'errorxreal_{ocupacao}.npz')
+        data_cnn = np.load(data_cnn_path, allow_pickle=True)
+        stats_por_intervalo_cnn = data_cnn['stats_por_intervalo'].item()
+        stats_por_intervalo_cnn_amplitude = data_cnn['stats_por_intervalo_amplitude'].item()
+
+        # Real Amplitude
+        data_real_amplitude_path = os.path.join(plots_path, "Dados", "RealAmplitude_ErrorxRealPhase", f'janelamento_{n_janelamento}', f'errorxreal_{ocupacao}.npz')
+        data_real_amplitude = np.load(data_real_amplitude_path, allow_pickle=True)
+        stats_por_intervalo_real_amplitude = data_real_amplitude['stats_por_intervalo'].item()
+        stats_por_intervalo_real_amplitude_ = data_real_amplitude['stats_por_intervalo_amplitude'].item()
+
+        if stats_por_intervalo_cnn8_amplitude and stats_por_intervalo_cnn_amplitude and stats_por_intervalo_real_amplitude_:
+            medias_cnn8 = [stats['media'] for stats in stats_por_intervalo_cnn8_amplitude.values()]
+            labels_cnn8 = list(stats_por_intervalo_cnn8_amplitude.keys())
+
+            medias_cnn = [stats['media'] for stats in stats_por_intervalo_cnn_amplitude.values()]
+            labels_cnn = list(stats_por_intervalo_cnn_amplitude.keys())
+
+            medias_real_amplitude = [stats['media'] for stats in stats_por_intervalo_real_amplitude_.values()]
+            labels_real_amplitude = list(stats_por_intervalo_real_amplitude_.keys())
+
+            ax[idx].plot(range(len(labels_cnn8)), medias_cnn8, marker='o', color=cnn8_color)
+            ax[idx].plot(range(len(labels_cnn)), medias_cnn, marker='o', color=cnn_color)
+            ax[idx].plot(range(len(labels_real_amplitude)), medias_real_amplitude, linestyle=':', marker='*', color=real_amplitude_color)
+            ax[idx].text(-0.15, 1.12, f'({chr(97+idx)})', transform=ax[idx].transAxes, fontsize=fontSize+6, va='top')
+            ax[idx].set_xticks(range(len(labels_cnn8)))
+            ax[idx].set_xticklabels(labels_cnn8, rotation=45, ha='right',fontsize=fontSize-17)
+            ax[idx].set_xlabel(f'Real Amplitude(ADC Counts)', fontsize=fontSize-8)
+            ax[idx].set_ylabel('Mean Error Values\nPhase Estimation (ns)', fontsize=fontSize-8)
+            ax[idx].grid(True, alpha=0.3)
+    handles = []
+    labels = []
+    
+    handles.append(plt.Line2D([0], [0], color=cnn8_color, linewidth=2))
+    labels.append('CNN-8')
+    handles.append(plt.Line2D([0], [0], color=real_amplitude_color, linewidth=2, linestyle=':', marker='*',))
+    labels.append('Real Amplitude')
+    cnn_types = ['CNN-3', 'CNN-5']
+    unique_cnns = list(set(cnn_types))
+    for cnn_type in unique_cnns:
+        if cnn_type == "CNN-5":
+            color = "#1A1A1A"
+        else: 
+            color = "#B0B0B0"
+       
+        handles.append(plt.Line2D([0], [0], color=color, linewidth=2))
+        labels.append(cnn_type)
+
+    fig.legend(
+        handles, labels,
+        loc='upper center',
+        ncol=len(handles),
+        bbox_to_anchor=(0.5, 0.9999),
+        frameon=False,
+        fontsize=fontSize
+    )
+    # plt.tight_layout()
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
+
+
+def PlotDispersions():
+    ocupacoes = [10,50,80,100]
+    total_inches_image = 6.32
+    fontSize = 24
+    fig, (ax) = plt.subplots(2, 2, figsize=(15, 6))
+    ax = ax.flatten()
+    cnn8_color = "#006130"
+    real_amplitude_color = "#FA3232"
+    base_path = os.path.dirname(os.path.dirname(path))
+    plots_path = os.path.join(base_path, "Plots")
+
+    for idx, ocupacao in enumerate(ocupacoes):
+        if ocupacao == 10 or ocupacao==50:
+            CNN = 5
+            cnn_color = "#1A1A1A"
+        elif ocupacao==80 or ocupacao==100:
+            CNN=3
+            cnn_color = "#B0B0B0"
+
+        # CNN8
+        data_cnn8_path = os.path.join(plots_path, "Dados", "CNN_ErrorxRealPhase", f'janelamento_{n_janelamento}', f'CNN_8', f'errorxreal_{ocupacao}.npz')
+        data_cnn8 = np.load(data_cnn8_path, allow_pickle=True)
+        stats_por_intervalo_cnn8 = data_cnn8['stats_por_intervalo'].item()
+        stats_por_intervalo_cnn8_amplitude = data_cnn8['stats_por_intervalo_amplitude'].item()
+        # # CNN
+        data_cnn_path = os.path.join(plots_path, "Dados", "CNN_ErrorxRealPhase", f'janelamento_{n_janelamento}', f'CNN_{CNN}', f'errorxreal_{ocupacao}.npz')
+        data_cnn = np.load(data_cnn_path, allow_pickle=True)
+        stats_por_intervalo_cnn = data_cnn['stats_por_intervalo'].item()
+        stats_por_intervalo_cnn_amplitude = data_cnn['stats_por_intervalo_amplitude'].item()
+
+        # Real Amplitude
+        data_real_amplitude_path = os.path.join(plots_path, "Dados", "RealAmplitude_ErrorxRealPhase", f'janelamento_{n_janelamento}', f'errorxreal_{ocupacao}.npz')
+        data_real_amplitude = np.load(data_real_amplitude_path, allow_pickle=True)
+        stats_por_intervalo_real_amplitude = data_real_amplitude['stats_por_intervalo'].item()
+        stats_por_intervalo_real_amplitude_ = data_real_amplitude['stats_por_intervalo_amplitude'].item()
+        
+        if stats_por_intervalo_cnn8_amplitude and stats_por_intervalo_cnn_amplitude and stats_por_intervalo_real_amplitude_:
+            desvios_cnn8 = [stats['std'] for stats in stats_por_intervalo_cnn8_amplitude.values()]
+            labels_cnn8 = list(stats_por_intervalo_cnn8_amplitude.keys())
+
+            desvios_cnn = [stats['std'] for stats in stats_por_intervalo_cnn_amplitude.values()]
+            labels_cnn = list(stats_por_intervalo_cnn_amplitude.keys())
+
+            desvios_real_amplitude = [stats['std'] for stats in stats_por_intervalo_real_amplitude_.values()]
+            labels_real_amplitude = list(stats_por_intervalo_real_amplitude_.keys())
+
+            ax[idx].plot(range(len(labels_cnn8)), desvios_cnn8, marker='o',   color=cnn8_color)
+            ax[idx].plot(range(len(labels_cnn)), desvios_cnn, marker='o',   color=cnn_color)
+            ax[idx].plot(range(len(labels_real_amplitude)), desvios_real_amplitude, linestyle=':', marker='*', color=real_amplitude_color)
+            ax[idx].set_xticks(range(len(labels_cnn8)))
+            ax[idx].set_xticklabels(labels_cnn8, rotation=45, ha='right',fontsize=fontSize-17)
+            ax[idx].text(-0.15, 1.12, f'({chr(97+idx)})', transform=ax[idx].transAxes, fontsize=fontSize+6, va='top')
+            ax[idx].set_xlabel(f'Real Amplitude(ADC Count)', fontsize = fontSize-8)
+            ax[idx].set_ylabel('Mean Dispersion Values\nPhase Estimation (ns)', fontsize = fontSize-8)
+            ax[idx].grid(True, alpha=0.3)
+    handles = []
+    labels = []
+    
+    handles.append(plt.Line2D([0], [0], color=cnn8_color, linewidth=2))
+    labels.append('CNN-8')
+    handles.append(plt.Line2D([0], [0], color=real_amplitude_color, linewidth=2, linestyle=':', marker='*',))
+    labels.append('Real Amplitude')
+    cnn_types = ['CNN-3', 'CNN-5']
+    unique_cnns = list(set(cnn_types))
+    for cnn_type in unique_cnns:
+        if cnn_type == "CNN-5":
+            color = "#1A1A1A"
+        else: 
+            color = "#B0B0B0"
+       
+        handles.append(plt.Line2D([0], [0], color=color, linewidth=2))
+        labels.append(cnn_type)
+
+    fig.legend(
+        handles, labels,
+        loc='upper center',
+        ncol=len(handles),
+        bbox_to_anchor=(0.5, 0.9999),
+        frameon=False,
+        fontsize=fontSize
+    )
+    # plt.tight_layout()
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
+
+PlotHistrogramasAmpitude()
+PlotHistrogramasPhase()
+PlotErros()
+PlotDispersions()
